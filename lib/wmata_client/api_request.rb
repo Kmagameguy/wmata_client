740945555747f82a59f1283066fed1520706ec88
Kevin A. <17864518+Kmagameguy@users.noreply.github.com>
2025-05-29 20:50:03 -0400

    Class Methods

--- lib/wmata_client/api_request.rb
+++ lib/wmata_client/api_request.rb
@@ -7,50 +7,52 @@ module WmataClient
 
     API_BASE_URL = "https://api.wmata.com"
 
-    def all_rail_lines
-      response = get("Rail.svc/json/jLines")&.dig("Lines") || []
-      response.map do |rail_lines_hash|
-        ::WmataClient::Models::RailLine.new(rail_lines_hash)
+    class << self
+      def all_rail_lines
+        response = get("Rail.svc/json/jLines")&.dig("Lines") || []
+        response.map do |rail_lines_hash|
+          ::WmataClient::Models::RailLine.new(rail_lines_hash)
+        end
       end
-    end
-    
-    def all_stations
-      response = get("Rail.svc/json/jStations")&.dig("Stations") || []
-      response.map do |station_hash|
-        ::WmataClient::Models::Station.new(station_hash)
+      
+      def all_stations
+        response = get("Rail.svc/json/jStations")&.dig("Stations") || []
+        response.map do |station_hash|
+          ::WmataClient::Models::Station.new(station_hash)
+        end
       end
-    end
 
-    def station_info(station_code:)
-      response = get("Rail.svc/json/jStationInfo", { "StationCode" => station_code })
-      ::WmataClient::Models::Station.new(response)
-    end
-
-    def station_schedule(station_code:)
-      response = get("Rail.svc/json/jStationTimes", { "StationCode" => station_code })&.dig("StationTimes") || []
-      response.map do |station_schedule_hash|
-        ::WmataClient::Models::StationSchedule.new(station_schedule_hash)
+      def station_info(station_code:)
+        response = get("Rail.svc/json/jStationInfo", { "StationCode" => station_code })
+        ::WmataClient::Models::Station.new(response)
       end
-    end
 
-    def get(path, opts={})
-      response = connection.get("#{API_BASE_URL}/#{path}", opts)
-      response&.body
-    end
-
-    def connection
-      @connection ||= Faraday::Connection.new do |f|
-        f.adapter Faraday.default_adapter
-        f.request :json
-        f.response :json
-        f.headers["api_key"] = api_key
-        f.headers["Content-Type"] = "application/json"
-        f.headers["Accept"] = "application/json"
+      def station_schedule(station_code:)
+        response = get("Rail.svc/json/jStationTimes", { "StationCode" => station_code })&.dig("StationTimes") || []
+        response.map do |station_schedule_hash|
+          ::WmataClient::Models::StationSchedule.new(station_schedule_hash)
+        end
       end
-    end
 
-    def api_key
-      ENV.fetch("WMATA_API_KEY")
+      def get(path, opts={})
+        response = connection.get("#{API_BASE_URL}/#{path}", opts)
+        response&.body
+      end
+
+      def connection
+        Faraday::Connection.new do |f|
+          f.adapter Faraday.default_adapter
+          f.request :json
+          f.response :json
+          f.headers["api_key"] = api_key
+          f.headers["Content-Type"] = "application/json"
+          f.headers["Accept"] = "application/json"
+        end
+      end
+
+      def api_key
+        ENV.fetch("WMATA_API_KEY")
+      end
     end
   end
 end
